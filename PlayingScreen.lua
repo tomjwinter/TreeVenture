5af1d3c4a48478bb9561b9da8c7a294c958d7caa
nolanwinter <winter.drummer.guy@gmail.com>
2015-01-12 19:40:07 -0600

    Display Correct Main Screen

--- PlayingScreen.lua
+++ PlayingScreen.lua
@@ -3,7 +3,7 @@ t = {}
 function t.open()
   print("HI")
   local group = display.newGroup()
-  local play = display.newImage(group, "Graphics/Playing Screen.jpg" )
+  local play = display.newImage(group, "Graphics/Playing Screen Boy.jpg" )
   play.x = 512; play.y = 384
   play.width = 1024; play.height = 768
   play.alpha = 0
