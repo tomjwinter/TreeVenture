7bb9890857c637244d54b6fdcac60dc5afb76a13
greifkat <wubberito@gmail.com>
2015-01-20 19:21:34 -0600

    kate test
    wanted to figure it out

--- PlayingScreen.lua
+++ PlayingScreen.lua
@@ -1,22 +1,30 @@
 t = {}
 
 function t.open()
+  print("HI")
   local group = display.newGroup()
   local play = display.newImage(group, "Graphics/Tree Game Background.jpg" )
   play.x = 512; play.y = 384
   play.width = 1024; play.height = 768
   play.alpha = 0
   transition.fadeIn( play )
+  
+  --Create an image of your character
+  -- and set the location and size
   local boy = display.newImage(group, "Graphics/Boy.png" )
   boy.x = 210; boy.y = 350
   boy.width = 100; boy.height = 190
+  
+  --Create an image of the D-Pad
+  -- and set the location and size
+  
   local tree = display.newImage(group, "Graphics/Transparent Tree.png" )
   tree.x = 710; tree.y = 320
   tree.width = 200; tree.height = 300
   --local treebutton = display.newRect(group, 710, 320, 200, 300 )
   --treebutton:setFillColor( 0, 0, 0.6, 0.2 )
   function treePress()
-    treeInfo.open()
+    notDone.open()
     group:removeSelf()
   end
   tree:addEventListener( "tap", treePress )
