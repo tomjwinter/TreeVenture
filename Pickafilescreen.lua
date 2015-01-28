31c1cf59a73cfc4f82f73fbad9790379df1986b2
greifkat <wubberito@gmail.com>
2015-01-27 18:36:46 -0600

    Added more comments to Pickafilescreen
    finished comments on this page finally
    
    Are you proud of me NOW Nolan?

--- Pickafilescreen.lua
+++ Pickafilescreen.lua
@@ -16,7 +16,7 @@ function t.open()
   local backButton = display.newRect(group, 940, 70, 150, 64 )
   backButton : setFillColor( 0, 0, 1, 0.2 )
   
-  --Creates a function
+  --This creates a function
   -- this function tells the device to go back to the startScreen
   function backPress()
     startScreen.open()
@@ -27,28 +27,38 @@ function t.open()
   -- when it gets the tap it does the backPress function
   backButton:addEventListener( "tap", backPress )
   
-  --This creates a rectangle
+  --This creates a rectangle for tree one
   -- and it sets the size, color, and location
   local treeButton1 = display.newRect(group, 150, 300, 225, 500 )
   treeButton1 : setFillColor( 0, 0, 1, 0.2 )
   
-  --This creates a rectangle
+  --This creates a rectangle for tree two
   -- and sets the size, color, and location
   local treeButton2 = display.newRect(group, 475, 465, 225, 500 )
   treeButton2 : setFillColor( 0, 0, 1, 0.2 )
   
-  --This creates a rectangle
+  --This creates a rectangle for tree three
   -- and sets the size, color, and location
   local treeButton3 = display.newRect(group, 825, 325, 225, 390 )
   treeButton3 : setFillColor( 0, 0, 1, 0.2 )
   
-  --
+  --This creates a function
+  -- this function tells the device to go to the genderScreen
   function treePress()
     genderScreen.open()
     group:removeSelf()
   end
+  
+  --This tells the device to listen for a tap on tree one
+  -- when it gets the tap it does the treePress function
   treeButton1:addEventListener( "tap", treePress )
+ 
+  --This tells the device to listen for a tap on tree two
+  -- when it gets the tapit does the treePress function
   treeButton2:addEventListener( "tap", treePress )
+  
+  --This tells the device to listen for a tap on tree three
+  -- when it gets the tap it does the treePress function
   treeButton3:addEventListener( "tap", treePress )
 end
 
