8cd45a363649c44430b2bd8bd99a13ee3b10ca0e
greifkat <wubberito@gmail.com>
2015-01-27 18:15:31 -0600

    Added Comments to the Pickafilescreen
    needed more comments on this page

--- Pickafilescreen.lua
+++ Pickafilescreen.lua
@@ -1,18 +1,30 @@
 local t = {}
 
 function t.open()
+  
+  --Displays pick a file screen 
+  -- and sets the location and size
   local group = display.newGroup()
   local file = display.newImage(group, "Graphics/Pick a File Screen.jpg" )
   file.x = 512; file.y = 384
   file.width = 1024; file.height = 768
   file.alpha = 0
   transition.fadeIn( file )
+  
+  --Displays back button
+  -- and sets location and size
   local backButton = display.newRect(group, 940, 70, 150, 64 )
   backButton : setFillColor( 0, 0, 1, 0.2 )
+  
+  --Creates a function
+  -- this function tells the device to go back to the startScreen
   function backPress()
     startScreen.open()
     group:removeSelf()
   end
+  
+  --This tells the device to listen for a tap on the backBbutton
+  -- when it gets the tap it does the backPress function
   backButton:addEventListener( "tap", backPress )
   local treeButton1 = display.newRect(group, 150, 300, 225, 500 )
   treeButton1 : setFillColor( 0, 0, 1, 0.2 )
