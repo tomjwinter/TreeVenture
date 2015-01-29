3c31d77250de992f0a487c04a5849a6946e7dcf4
greifkat <wubberito@gmail.com>
2015-01-28 20:05:01 -0600

    added rectangles
    5 more to go

--- TreeInfo.lua
+++ TreeInfo.lua
@@ -18,12 +18,28 @@ function t.open()
   --Adds Tree
   -- and sets the size and location
   local tree = display.newImage(group, "Graphics/Transparent Tree.png" )
-  tree.x = 612; tree.y = 384
-  tree.width = 800; tree.height = 750
+  tree.x = 675; tree.y = 384
+  tree.width = 700; tree.height = 700
   
-  --Adds a rectangle
+  --Adds a rectangle for the word roots
   -- and sets the size and location
-  local rootsBox = display.newRect(group, 635, 715, 315, 60 )
-end
+  local rootsBox = display.newRect(group, 200, 715, 300, 75 )
+  
+  --Adds a rectangle for the word trunk
+  -- and sets the size and location
+  local trunkBox = display.newRect(group, 200, 600, 300, 75 )
+  
+  --Adds a rectangle for the bark word
+  -- and sets location and size
+  local barkBox = display.newRect(group, 200, 485, 300, 75 )
+ 
+ --Adds a rectangle for the word branches
+ -- and sets the size and location
+ local branchesBox = display.newRect(group, 200, 370, 300, 75 )
+ 
+ --Adds a rectangle for the word leaves
+ -- and sets the size and location
+ local leavesBox = display.newRect(group, 200, 255, 300, 75 )
+    end
 
 return t
