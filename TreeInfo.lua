bcd40f3c706d641a466cb244c7f54d93e0cd9fc8
greifkat <wubberito@gmail.com>
2015-02-01 12:02:54 -0600

    Finished boxes both blank and for the words
    i think i got them all in the rite place if i didn't we can change it
    later

--- TreeInfo.lua
+++ TreeInfo.lua
@@ -29,7 +29,7 @@ function t.open()
   -- and sets the size and location
   local trunkBox = display.newRect(group, 200, 600, 300, 75 )
   
-  --Adds a rectangle for the bark word
+  --Adds a rectangle for the word bark
   -- and sets location and size
   local barkBox = display.newRect(group, 200, 485, 300, 75 )
  
@@ -40,6 +40,26 @@ function t.open()
  --Adds a rectangle for the word leaves
  -- and sets the size and location
  local leavesBox = display.newRect(group, 200, 255, 300, 75 )
-    end
+ 
+ --Adds blank rectangle for roots
+ -- and sets location and size
+ local blankRootsBox = display.newRect(group, 685, 700, 300, 75 )
+ 
+ --Adds blank rectangle for the trunk
+ -- and sets location and size
+ local blankTrunkBox = display.newRect(group, 700, 550, 125, 200 )
+ 
+ --Adds blank rectangle for bark
+ -- and sets the location and size
+ local blankBarkBox = display.newRect(group, 900, 550, 225, 75 )
+ 
+ --Adds blank rectangle for branches
+ -- and sets the location and size
+ local blankBranchesBox = display.newRect(group, 675, 275, 200, 90 )
+ 
+ --Adds a blank rectangle for leaves
+ -- and sets the location and size
+ local blankLeavesBox = display.newRect(group, 850, 150, 250, 75 )
+   end
 
 return t
