fdb11fa868f5f61a19e411792e8762abdaa085e2
adhdluke <adhdluke@outlook.com>
2015-02-04 19:41:59 -0600

    hwahwahwa added a ton of stoof :D
    :D:D:D:D:D:D:D:D:D:D:D:D:D:D

--- treegame1.lua
+++ treegame1.lua
@@ -7,5 +7,16 @@ local group = display.newGroup()
   play.width = 1024; play.height = 768
   play.alpha = 0
   transition.fadeIn( play )
-  end
+   local tree = display.newImage(group, "Graphics/Transparent Tree.png" )
+  tree.x = 800; tree.y = 400
+  tree.width = 520; tree.height = 620
+  local branch = display.newImage(group, "Graphics/Tap The Blank.png" )
+  branch.x = 150; branch.y = 250
+  branch.width = 350; branch.height = 375
+  local backpack = display.newImage(group, "Graphics/BackPack for collecting apple minigame.png" )
+  backpack.x = 250; backpack.y = 600
+  backpack.width = 800; backpack.height = 425
+  
+end
+
   return t
