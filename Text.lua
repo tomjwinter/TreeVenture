62889559a1715901b63ae75f4495120e412857a4
Zach <elizabethgeissler@yahoo.com>
2015-02-19 20:28:45 -0600

    Added right text

--- Text.lua
+++ Text.lua
@@ -1,9 +1,9 @@
 t = {}
 
-t.rootsString = "The Roots absorb water and nutrients from the soil."
-t.branchesString = "The branches hold the leaves as well as breathe in oxygen."
-t.leavesString = "The leaves make sugar and send it to the rest of the tree."
-t.barkString = "The bark protects the tree. The inner bark takes food from the leaves to the rest of the tree."
-t.trunkString = "The trunk supports the rest of the tree."
+t.rootsString = "The Roots absorb water and nutrients from the soil. They also keep the tree grounded."
+t.branchesString = "Branches carry the nutrients from the trunk to the leaves. They also hold the leaves and breathe in oxygen."
+t.leavesString = "The leaves make sugar and send it to the rest of the tree. They also release oxygen for us to breathe."
+t.barkString = "The bark protects the tree. The inner bark takes the food from the leaves to the rest of the tree."
+t.trunkString = "The trunk takes the nutrients from the roots to the branches. It also supports the rest of the tree and makes it strong."
 
 return t
