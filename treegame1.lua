eb6664335000b7a291c67629c9f0354780fb7263
Tom Winter <tomjwinter@gmail.com>
2015-02-19 23:19:37 -0600

    Reduce shading color

--- treegame1.lua
+++ treegame1.lua
@@ -54,7 +54,7 @@ function t.open()
   local tree = display.newImage(group, "Graphics/Transparent Tree.png" )
   tree.x = 800; tree.y = 400
   tree.width = 520; tree.height = 620
-  local branch = display.newImage(group, "Graphics/Tap The BRANCH.png" )
+  local branch = display.newImage(group, "Graphics/Tap the BRANCH.png" )
   branch.x = 150; branch.y = 250
   branch.width = 350; branch.height = 375
   nextQuiz()
@@ -70,29 +70,29 @@ function t.open()
   -- Add the tap rectangles for the tree
   -- Leaves
   local leaves1 = display.newRect ( 800, 180, 450, 180 )
-  leaves1:setFillColor( 0, 0, 1, .2 )
+  leaves1:setFillColor( 0, 0, 1, .05 )
   leaves1:addEventListener( "tap", correctTap )
   leaves1.item = "Leaves"
   
   local leaves2 = display.newRect ( 970, 360, 120, 180 )
-  leaves2:setFillColor( 0, 0, 1, .2 )
+  leaves2:setFillColor( 0, 0, 1, .05 )
   leaves2:addEventListener( "tap", correctTap )
   leaves2.item = "Leaves"
 
   local leaves3 = display.newRect ( 610, 380, 80, 220 )
-  leaves3:setFillColor( 0, 0, 1, .2 )
+  leaves3:setFillColor( 0, 0, 1, .05 )
   leaves3:addEventListener( "tap", correctTap )
   leaves3.item = "Leaves"
 
   -- Branches
   local branches = display.newRect ( 780, 340, 260, 140 )
-  branches:setFillColor( 0, 0, 1, .2 )
+  branches:setFillColor( 0, 0, 1, .05 )
   branches:addEventListener( "tap", correctTap )
   branches.item = "Branches"
   
   -- Trunk
   local trunk = display.newRect ( 800, 520, 170, 220 )
-  trunk:setFillColor( 0, 0, 1, .2 )
+  trunk:setFillColor( 0, 0, 1, .05 )
   trunk:addEventListener( "tap", correctTap )
   trunk.item = "Trunk"
   
@@ -102,7 +102,7 @@ function t.open()
 
   -- Roots
   local roots = display.newRect ( 800, 670, 300, 80 )
-  roots:setFillColor( 0, 0, 1, .2 )
+  roots:setFillColor( 0, 0, 1, .05 )
   roots:addEventListener( "tap", correctTap )
   roots.item = "Roots"
 
